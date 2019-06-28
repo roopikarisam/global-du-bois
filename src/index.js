264c93daab1753dfdc0b1a243521efcbe2faae15
Roopika Risam <rrisam@salemstate.edu>
2019-06-27 22:38:54 -0400

    testing D3 selectors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
 console.log("working");
-const square = d3.selectAll("circle");
-circle.style("fill", "blue");
+const circle = d3.selectAll("circle");
+circle.style("fill", "steel blue");
 circle.attr("r", 30);
